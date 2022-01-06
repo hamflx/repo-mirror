31d80e11947f6f4679e1c7751a80409457a5d83b
hamflx <a@hamflx.cn>
2022-01-06 19:20:56 +0800

    feat: 作为守护进程运行

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::collections::HashSet;
-use std::str;
+use std::time::Duration;
 use std::{env, fs, path::Path};
+use std::{str, thread};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use env_logger::Env;
 use git2::{
     build::RepoBuilder, Cred, CredentialType, FetchOptions, PushOptions, RemoteCallbacks,
     Repository,
 };
+use log::{info, warn};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -21,9 +24,31 @@ struct KnownHosts {
 }
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let (mirrors_dir, repos) = read_sync_repos();
+    let known_hosts = KnownHosts::load().unwrap_or_else(|_| KnownHosts::new());
+    let (mut fetch_opts, mut push_opts, mut builder) = new_git_network_opts(known_hosts);
+
+    loop {
+        if let Err(err) = do_sync(
+            &repos,
+            mirrors_dir,
+            &mut builder,
+            &mut fetch_opts,
+            &mut push_opts,
+        ) {
+            warn!("An error occurred: {}", err);
+        }
+
+        info!("waiting for next tick");
+        thread::sleep(Duration::from_secs(30 * 60));
+    }
+}
+
+fn read_sync_repos() -> (&'static str, Vec<SyncRepository>) {
     let mirrors_dir = r#"D:\tmp\mirrors"#;
     fs::create_dir_all(mirrors_dir).unwrap();
-
     let repos: Vec<SyncRepository> = serde_json::from_str(
         str::from_utf8(&fs::read("repos.json").expect("无法读取 repos.json"))
             .expect("文件内容不是有效的 utf8 格式"),
@@ -32,13 +57,16 @@ fn main() {
     if repos.is_empty() {
         panic!("未找到有效的仓库配置项");
     }
+    (mirrors_dir, repos)
+}
 
-    let mut known_hosts = KnownHosts::load().unwrap_or_else(|_| KnownHosts::new());
-
+fn new_git_network_opts<'cb>(
+    mut known_hosts: KnownHosts,
+) -> (FetchOptions<'cb>, PushOptions<'cb>, RepoBuilder<'cb>) {
     let mut clone_callbacks = RemoteCallbacks::new();
     let mut clone_opts = FetchOptions::new();
     clone_callbacks.credentials(get_credentials);
-    clone_callbacks.certificate_check(|cert, host| {
+    clone_callbacks.certificate_check(move |cert, host| {
         let host_key = base64::encode(cert.as_hostkey().unwrap().hash_sha256().unwrap());
         if known_hosts.check(&host_key) {
             return true;
@@ -75,28 +103,39 @@ fn main() {
     builder.fetch_options(clone_opts);
     builder.bare(true);
 
-    for sync_repo in &repos {
-        let repo_name = sync_repo.source.split('/').last().unwrap();
+    (fetch_opts, push_opts, builder)
+}
+
+fn do_sync(
+    repos: &Vec<SyncRepository>,
+    mirrors_dir: &str,
+    builder: &mut RepoBuilder,
+    fetch_opts: &mut FetchOptions,
+    push_opts: &mut PushOptions,
+) -> Result<()> {
+    for sync_repo in repos {
+        let repo_name = sync_repo
+            .source
+            .split('/')
+            .last()
+            .ok_or_else(|| anyhow!("仓库地址应为非空字符串"))?;
+        info!("syncing {}", repo_name);
+
         let repo_dir_path_buf = Path::new(mirrors_dir).join(repo_name);
         let repo_dir_path = repo_dir_path_buf.as_path();
         let repo = if repo_dir_path.exists() {
-            Repository::open(repo_dir_path).unwrap()
+            Repository::open(repo_dir_path)?
         } else {
-            builder
-                .clone(sync_repo.source.as_str(), repo_dir_path)
-                .unwrap()
+            (*builder).clone(sync_repo.source.as_str(), repo_dir_path)?
         };
-        let mut remote_origin = repo.find_remote("origin").unwrap();
+        let mut remote_origin = repo.find_remote("origin")?;
         let mut remote_mirror = repo
             .find_remote("mirror")
-            .or_else(|_| repo.remote("mirror", sync_repo.mirror.as_str()))
-            .unwrap();
+            .or_else(|_| repo.remote("mirror", sync_repo.mirror.as_str()))?;
 
-        remote_origin
-            .fetch(&["+refs/heads/*:refs/heads/*"], Some(&mut fetch_opts), None)
-            .unwrap();
+        remote_origin.fetch(&["+refs/heads/*:refs/heads/*"], Some(fetch_opts), None)?;
 
-        let origin_refs = remote_origin.list().unwrap();
+        let origin_refs = remote_origin.list()?;
         let origin_heads: Vec<_> = origin_refs
             .iter()
             .filter(|s| s.name().starts_with("refs/heads/"))
@@ -107,12 +146,12 @@ fn main() {
             .map(|s| format!("{}:{}", s, s))
             .collect::<Vec<_>>();
 
-        remote_mirror
-            .push(push_refspecs.as_slice(), Some(&mut push_opts))
-            .unwrap();
+        remote_mirror.push(push_refspecs.as_slice(), Some(push_opts))?;
 
-        repo.remote_delete("mirror").unwrap();
+        repo.remote_delete("mirror")?;
     }
+
+    Ok(())
 }
 
 fn get_credentials(
